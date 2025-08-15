b1179cad5c78bb5301382c158731645149f619a3
verticul <kelvinakinolaajayi@hotmail.com>
2025-08-15 21:20:59 +0100

    Adding the list components and refactoring code

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // - - GLOBAL - -
+const MAX_CHARS = 150;
+
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
+const feedbackListEl  = document.querySelector('.feedbacks');
+const submitBtnEl  = document.querySelector('.submit-btn');
 
 // - - COUNTER COMPONENT - - //
 const inputHandler = () => {
     // Determine the maxmimum number of characters
-    const maxNrsChars = 150;
+    const maxNrsChars = MAX_CHARS;
 
     // Determine the number of characters currently typed
     const nrCharsTyped = textareaEl.value.length;
@@ -24,7 +28,20 @@ const inputHandler = () => {
 textareaEl.addEventListener('input', inputHandler);
 
 
-// - - SUBMIT COMPONENT - - //
+// - - FORM COMPONENT - - //
+
+const showVisualIndicator = (textCheck) => {
+    const className = textCheck === 'valid' ? 'form--valid' : 'form--invalid';
+
+    // Show the valid form submission visual indicator
+    formEl.classList.add(className);
+
+    //Remove the valid form submission visual indicator
+    setTimeout(() => {
+        formEl.classList.remove(className);
+    }, 2000);
+};
+
 const submitHandler = event => {
     // Prevent the default browser action (Submitting form data to the 'action'-address and refreshing page)
     event.preventDefault();
@@ -34,21 +51,9 @@ const submitHandler = event => {
     
     // Validate text (e.g. check if hashtag is present and the text is long enough)
     if (text.includes('#') && text.length >= 5) {
-        // Show the valid form submission visual indicator
-        formEl.classList.add('form--valid');
-
-        //Remove the valid form submission visual indicator
-        setTimeout(() => {
-            formEl.classList.remove('form--valid');
-        }, 2000);
+        showVisualIndicator('valid');
     } else {
-        // Show the invalid form submission visual indicator
-        formEl.classList.add('form--invalid');
-
-        setTimeout(() => {
-            //Remove the invalid form submission visual indicator
-            formEl.classList.remove('form--invalid');
-        }, 2000);
+        showVisualIndicator('invalid');
 
         // Focus on the text area again
         textareaEl.focus();
@@ -60,11 +65,42 @@ const submitHandler = event => {
     // We have text, now we need to extract other info from the text
     const hashtag = text.split(' ').find(word => word.includes('#'));
     const company = hashtag.substring(1);
-    const badgeLetter = company.substring(0, 1).toUpperCase;
+    const badgeLetter = company.substring(0, 1).toUpperCase();
     const upvoteCount = 0;
-    const days Ago = 0;
+    const daysAgo = 0;
 
-    
+    // New feedback item HTML
+    const feedbackItemHTML = `
+        <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${company}</p>
+                <p class="feedback__text">${text}</p>
+            </div>
+            <p class="feedback__date">${daysAgo === 0 ? 'NEW' : `${daysAgo}d`}</p>
+        </li>
+
+    `;
+
+    // Insert the HTML item into the unordered list
+    feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+
+    // Clear the text area after submission
+    textareaEl.value = '';
+
+    // Blur the submit button after submission
+    submitBtnEl.blur();
+
+    // Reset the counter after submission
+    counterEl.textContent = MAX_CHARS;
+
+    console.log(counterEl);
 };
 
 formEl.addEventListener('submit', submitHandler);
