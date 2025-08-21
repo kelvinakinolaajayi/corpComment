f9851437b1d0ad173c6e8e1e9a22ce8ec230c00f
verticul <kelvinakinolaajayi@hotmail.com>
2025-08-21 10:53:21 +0100

    Creating company filters and upvote count

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // - - GLOBAL - -
 const MAX_CHARS = 150;
+const BASE_API_URL = 'https://bytegrad.com/course-assets/js/1/api';
+
 
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
@@ -7,6 +9,31 @@ const formEl = document.querySelector('.form');
 const feedbackListEl  = document.querySelector('.feedbacks');
 const submitBtnEl  = document.querySelector('.submit-btn');
 const spinnerEl  = document.querySelector('.spinner');
+const hastagListEl  = document.querySelector('.hashtags');
+
+const renderFeedbackItem = feedbackItem => {
+       // New feedback item HTML
+       const feedbackItemHTML = `
+       <li class="feedback">
+           <button class="upvote">
+               <i class="fa-solid fa-caret-up upvote__icon"></i>
+               <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+           </button>
+           <section class="feedback__badge">
+               <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+           </section>
+           <div class="feedback__content">
+               <p class="feedback__company">${feedbackItem.company}</p>
+               <p class="feedback__text">${feedbackItem.text}</p>
+           </div>
+           <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
+       </li>
+
+   `;
+
+   // Insert the HTML item into the unordered list
+   feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+};
 
 // - - COUNTER COMPONENT - - //
 const inputHandler = () => {
@@ -70,27 +97,32 @@ const submitHandler = event => {
     const upvoteCount = 0;
     const daysAgo = 0;
 
-    // New feedback item HTML
-    const feedbackItemHTML = `
-        <li class="feedback">
-            <button class="upvote">
-                <i class="fa-solid fa-caret-up upvote__icon"></i>
-                <span class="upvote__count">${upvoteCount}</span>
-            </button>
-            <section class="feedback__badge">
-                <p class="feedback__letter">${badgeLetter}</p>
-            </section>
-            <div class="feedback__content">
-                <p class="feedback__company">${company}</p>
-                <p class="feedback__text">${text}</p>
-            </div>
-            <p class="feedback__date">${daysAgo === 0 ? 'NEW' : `${daysAgo}d`}</p>
-        </li>
+    // Render feedback item in list
+    const feedbackItem = {
+        upvoteCount,
+        company,
+        badgeLetter,
+        daysAgo,
+        text
+    };
+    renderFeedbackItem(feedbackItem);
 
-    `;
-
-    // Insert the HTML item into the unordered list
-    feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+    // Send feedback items to the server
+    fetch(`${BASE_API_URL}/feedbacks`, {
+        method: 'POST',
+        body: JSON.stringify(feedbackItem),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if (!response.ok) {
+            console.log('Something went wrong');
+            return;
+        } 
+        
+        console.log('Sucessfully submitted');
+    }).catch(error => console.log(error));
 
     // Clear the text area after submission
     textareaEl.value = '';
@@ -107,40 +139,80 @@ const submitHandler = event => {
 formEl.addEventListener('submit', submitHandler);
 
 // - - FEEDBACK LIST COMPONENT -- //
-fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
+const clickHandler = event => {
+    // Get the HTML element which has been clicked
+    const clickedEl = event.target;
+
+    // Determine if user intended to upvote or epand the comment
+    const upvoteIntention = clickedEl.className.includes('upvote');
+
+    // Run the appropriate logic for the two options above
+    if (upvoteIntention) {
+        // Get the closest upvote button
+        const upvoteBtnEl = clickedEl.closest('.upvote');
+
+        // Disable upvote button (prevents double clicks, spam)
+        upvoteBtnEl.disabled = true;
+
+        // Select the upvote count element within the upvote button
+        const upvoteCountEl = upvoteBtnEl.querySelector('.upvote__count');
+
+        // Get the current displayed upvote count number
+        let upvoteCount = +upvoteCountEl.textContent;
+
+        // Set upvote count incremented by 1
+        upvoteCountEl.textContent = ++upvoteCount;
+
+        console.log(typeof upvoteCount);
+    } else {
+        // Expand the clicked feedback Item - A way to target specific list items
+        clickedEl.closest('.feedback').classList.toggle('feedback--expand');
+    }
+
+};
+
+feedbackListEl.addEventListener('click', clickHandler);
+
+fetch(`${BASE_API_URL}/feedbacks`)
     .then(response => response.json())
     .then(data => {
         // Removing the loading spinner animation
         spinnerEl.remove();
 
         // Iterate over each element in the feedbacks array and render it in list
-        data.feedbacks.forEach(feedbackItem => {
-            // New feedback item HTML
-            const feedbackItemHTML = `
-            <li class="feedback">
-                <button class="upvote">
-                    <i class="fa-solid fa-caret-up upvote__icon"></i>
-                    <span class="upvote__count">${feedbackItem.upvoteCount}</span>
-                </button>
-                <section class="feedback__badge">
-                    <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
-                </section>
-                <div class="feedback__content">
-                    <p class="feedback__company">${feedbackItem.company}</p>
-                    <p class="feedback__text">${feedbackItem.text}</p>
-                </div>
-                <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
-            </li>
-    
-        `;
-    
-        // Insert the HTML item into the unordered list
-        feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
-        
-    });
+        data.feedbacks.forEach(feedbackItem => renderFeedbackItem(feedbackItem));
 
 })
 .catch(error => {
     feedbackListEl.textContent = `Failed to fetch feedback items. Error message: ${error.message}`;
 })
 
+// - - HASHTAG LIST COMPONENT - - 
+const clickHandler2 = event => {
+    // Get the clicked element
+    const clickedEl = event.target;
+
+    // Stop function if click happens in list, but not outside buttons
+    if (clickedEl.className === 'hashtags') return;
+
+    // Extract the actual company name that was clicked
+    const companyNameFromHashtag = clickedEl.textContent.substring(1).toLowerCase().trim();
+
+    // Iterate over each feedback item in the list
+    feedbackListEl.childNodes.forEach(childNode => {
+        // Stop this iteration if it's a text node
+        if (childNode.nodeType === 3) return;
+
+        // Extract company name from the li
+        const companyNameFromFeedbackItem = childNode.querySelector('.feedback__company').textContent.toLowerCase().trim();
+
+        // Remove feedback items from list if company names are not equal
+        if (companyNameFromHashtag !== companyNameFromFeedbackItem) {
+            childNode.remove();
+        }
+    });
+
+    console.log(clickedEl);
+};
+
+hastagListEl.addEventListener('click', clickHandler2)
